39922874733466a85015a81c139e9c068d2a80e8
Mathieu Lemay <acidrain1@gmail.com>
2025-08-24 15:44:07 -0400

    Use single struct for Result

--- safetypes/result.go
+++ safetypes/result.go
@@ -15,10 +15,6 @@ type Result[T any] interface {
 	IsErr() bool
 	// IsErrAnd returns `true` if the result is Err and the value inside of it matches a predicate.
 	IsErrAnd(f func(error) bool) bool
-	// Inspect calls a function with a reference to the contained value if Ok. Returns the original result.
-	Inspect(f func(*T)) Result[T]
-	// InspectErr calls a function with a reference to the contained value if Err. Returns the original result.
-	InspectErr(f func(error)) Result[T]
 	// Expect returns the contained Ok value, consuming the self value. Panics if the value is an Err, with a panic
 	// message including the passed message, and the content of the Err.
 	Expect(msg string) T
@@ -37,6 +33,10 @@ type Result[T any] interface {
 	// UnwrapErr returns the contained Err value, consuming the self value. Panics if the value is an Ok, with a custom
 	// panic message provided by the Ok's value.
 	UnwrapErr() error
+	// Inspect calls a function with a reference to the contained value if Ok. Returns the original result.
+	Inspect(f func(*T)) Result[T]
+	// InspectErr calls a function with a reference to the contained value if Err. Returns the original result.
+	InspectErr(f func(error)) Result[T]
 	// AsOptionValue converts a Result to a Some when res is result.Ok or None when res is result.Err.
 	AsOptionValue() Option[T]
 	// AsOptionErr converts a Result to a Some when res is result.Err or None when res is result.Ok.
@@ -51,40 +51,52 @@ type Result[T any] interface {
 	fmt.Stringer
 }
 
+// Ok creates an Ok variant of Result from the value.
+func Ok[T any](val T) Result[T] {
+	return &result[T]{
+		ok:  true,
+		val: val,
+		err: nil,
+	}
+}
+
+// Err creates an Err variant of Result from the error.
+func Err[T any](err error) Result[T] {
+	var v T
+
+	return &result[T]{
+		ok:  false,
+		val: v,
+		err: err,
+	}
+}
+
 // ResultOf creates a Result from the given value and error.
 func ResultOf[T any](val T, err error) Result[T] {
 	if !reflect.ValueOf(&err).Elem().IsNil() {
-		return &errT[T]{
-			err: err,
-		}
+		return Err[T](err)
 	}
 
-	return &ok[T]{
-		val: val,
-	}
+	return Ok(val)
 }
 
 // MapResult maps a Result<T> to Result<U, E> by applying a function to a contained Ok value, leaving an Err value
 // untouched.
 func MapResult[T any, U any](res Result[T], f func(T) U) Result[U] {
-	s, isOk := res.(*ok[T])
-	if !isOk {
-		return &errT[U]{res.UnwrapErr()}
+	if res.IsErr() {
+		return Err[U](res.UnwrapErr())
 	}
 
-	val := f(s.val)
-
-	return &ok[U]{val}
+	return Ok(f(res.Unwrap()))
 }
 
 // MapResultOr returns the provided default (if Err), or applies a function to the contained value (if Ok).
 func MapResultOr[T any, U any](res Result[T], def U, f func(T) U) U {
-	s, isOk := res.(*ok[T])
-	if !isOk {
+	if res.IsErr() {
 		return def
 	}
 
-	return f(s.val)
+	return f(res.Unwrap())
 }
 
 // MapResultOrElse maps a Result<T> to U by applying fallback function default to a contained Err value, or function
@@ -94,21 +106,151 @@ func MapResultOrElse[T any, U any](
 	factory func() U,
 	mapper func(T) U,
 ) U {
-	s, isOk := res.(*ok[T])
-	if !isOk {
+	if res.IsErr() {
 		return factory()
 	}
 
-	return mapper(s.val)
+	return mapper(res.Unwrap())
 }
 
 // MapResultErr maps a Result<T> to Result<T, F> by applying a function to a contained Err value, leaving an Ok value
 // untouched.
 func MapResultErr[T any](res Result[T], f func(error) error) Result[T] {
-	s, isOk := res.(*ok[T])
-	if isOk {
-		return s
+	if res.IsOk() {
+		return res
 	}
 
-	return &errT[T]{f(res.UnwrapErr())}
+	return Err[T](f(res.UnwrapErr()))
+}
+
+type result[T any] struct {
+	ok  bool
+	val T
+	err error
+}
+
+func (r *result[T]) IsOk() bool {
+	return r.ok
+}
+
+func (r *result[T]) IsOkAnd(f func(T) bool) bool {
+	return r.ok && f(r.val)
+}
+
+func (r *result[T]) IsErr() bool {
+	return !r.ok
+}
+
+func (r *result[T]) IsErrAnd(f func(error) bool) bool {
+	return !r.ok && f(r.err)
+}
+
+func (r *result[T]) Inspect(f func(*T)) Result[T] {
+	if r.ok {
+		f(&r.val)
+	}
+
+	return r
+}
+
+func (r *result[T]) InspectErr(f func(error)) Result[T] {
+	if !r.ok {
+		f(r.err)
+	}
+
+	return r
+}
+
+func (r *result[T]) Expect(msg string) T {
+	if r.ok {
+		return r.val
+	}
+
+	panic(fmt.Errorf("%s: %w", msg, r.err))
+}
+
+func (r *result[T]) ExpectErr(msg string) error {
+	if !r.ok {
+		return r.err
+	}
+
+	panic(fmt.Errorf("%s: %v", msg, r.val))
+}
+
+func (r *result[T]) Unwrap() T {
+	if r.ok {
+		return r.val
+	}
+
+	panic(fmt.Errorf("called `Result.Unwrap()` on an `Err` value: %w", r.err))
+}
+
+func (r *result[T]) UnwrapOr(def T) T {
+	if r.ok {
+		return r.val
+	}
+
+	return def
+}
+
+func (r *result[T]) UnwrapOrElse(f func() T) T {
+	if r.ok {
+		return r.val
+	}
+
+	return f()
+}
+
+func (r *result[T]) UnwrapOrDefault() T {
+	if r.ok {
+		return r.val
+	}
+
+	var def T
+
+	return def
+}
+
+func (r *result[T]) UnwrapErr() error {
+	if r.ok {
+		panic(fmt.Errorf("called `Result.UnwrapErr()` on an `Ok` value: %v", r.val))
+	}
+
+	return r.err
+}
+
+func (r *result[T]) AsOptionValue() Option[T] {
+	if r.ok {
+		return Some(r.val)
+	}
+
+	return None[T]()
+}
+
+func (r *result[T]) AsOptionErr() Option[error] {
+	if !r.ok {
+		return Some(r.err)
+	}
+
+	return None[error]()
+}
+
+func (r *result[T]) WrapErr(msg string) Result[T] {
+	if !r.ok {
+		return Err[T](fmt.Errorf("%s: %w", msg, r.err))
+	}
+
+	return r
+}
+
+func (r *result[T]) Expand() (T, error) {
+	return r.val, r.err
+}
+
+func (r *result[T]) String() string {
+	if r.ok {
+		return fmt.Sprintf("Ok(%v)", r.val)
+	}
+
+	return fmt.Sprintf("Err(%v)", r.err)
 }
--- safetypes/result_test.go
+++ safetypes/result_test.go
@@ -1,11 +1,13 @@
 package st
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type MockError struct {
@@ -63,7 +65,7 @@ func TestMapResult_ReturnsANewResultWithMappedResultValue(t *testing.T) {
 			return 0
 		}
 
-		expected := &errT[int]{err}
+		expected := Err[int](err)
 
 		assert.Equal(t, expected, MapResult(e, f))
 	})
@@ -141,7 +143,7 @@ func TestMapResultErr_ReturnsANewResultWithMappedResultValue(t *testing.T) {
 			return nil
 		}
 
-		expected := &ok[int]{value}
+		expected := Ok[int](value)
 
 		assert.Equal(t, expected, MapResultErr(o, f))
 	})
@@ -157,8 +159,439 @@ func TestMapResultErr_ReturnsANewResultWithMappedResultValue(t *testing.T) {
 			return newE
 		}
 
-		expected := &errT[any]{err: newE}
+		expected := Err[any](newE)
 
 		assert.Equal(t, expected, MapResultErr(e, f))
 	})
+}
+
+func TestResult_IsOk(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.True(t, r.IsOk())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		assert.False(t, r.IsOk())
+	})
+}
+
+func TestResult_IsOkAnd(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		for _, res := range []bool{true, false} {
+			name := fmt.Sprintf("predicate returns %v", res)
+			t.Run(name, func(t *testing.T) {
+				called := false
+
+				f := func(v int) bool {
+					called = true
+
+					assert.Equal(t, val, v)
+
+					return res
+				}
+
+				assert.Equal(t, res, r.IsOkAnd(f))
+				assert.True(t, called, "predicate should have been called")
+			})
+		}
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		for _, res := range []bool{true, false} {
+			name := fmt.Sprintf("predicate returns %v", res)
+			t.Run(name, func(t *testing.T) {
+				f := func(int) bool {
+					assert.Fail(t, "predicate should not have been called")
+
+					return res
+				}
+
+				assert.False(t, r.IsOkAnd(f))
+			})
+		}
+	})
+}
+
+func TestResult_IsErr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.False(t, r.IsErr())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		assert.True(t, r.IsErr())
+	})
+}
+
+func TestResult_IsErrAnd(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		for _, res := range []bool{true, false} {
+			name := fmt.Sprintf("predicate returns %v", res)
+			t.Run(name, func(t *testing.T) {
+				f := func(error) bool {
+					assert.Fail(t, "predicate should not have been called")
+
+					return res
+				}
+
+				assert.False(t, r.IsErrAnd(f))
+			})
+		}
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		for _, res := range []bool{true, false} {
+			name := fmt.Sprintf("predicate returns %v", res)
+			t.Run(name, func(t *testing.T) {
+				called := false
+
+				f := func(e error) bool {
+					called = true
+
+					assert.Equal(t, err, e)
+
+					return res
+				}
+
+				assert.Equal(t, res, r.IsErrAnd(f))
+				assert.True(t, called, "predicate should have been called")
+			})
+		}
+	})
+}
+
+func TestResult_Expect(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Equal(t, val, r.Expect(fake.RandomStringWithLength(8)))
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		msg := fake.RandomStringWithLength(8)
+		expectedError := fmt.Errorf("%s: %w", msg, err)
+		assert.PanicsWithError(t, expectedError.Error(), func() {
+			r.Expect(msg)
+		})
+	})
+}
+
+func TestResult_ExpectErr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		msg := fake.RandomStringWithLength(8)
+		expectedError := fmt.Errorf("%s: %v", msg, val)
+		assert.PanicsWithError(t, expectedError.Error(), func() {
+			_ = r.ExpectErr(msg)
+		})
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		assert.Equal(t, err, r.ExpectErr(fake.RandomStringWithLength(8)))
+	})
+}
+
+func TestResult_Unwrap(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Equal(t, val, r.Unwrap())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		expected := fmt.Errorf("called `Result.Unwrap()` on an `Err` value: %w", err)
+		assert.PanicsWithError(t, expected.Error(), func() {
+			_ = r.Unwrap()
+		})
+	})
+}
+
+func TestResult_UnwrapOr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Equal(t, val, r.UnwrapOr(fake.Int()))
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		def := fake.Int()
+
+		assert.Equal(t, def, r.UnwrapOr(def))
+	})
+}
+
+func TestResult_UnwrapOrElse(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		f := func() int {
+			assert.Fail(t, "should not have been called")
+
+			return fake.Int()
+		}
+
+		assert.Equal(t, val, r.UnwrapOrElse(f))
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		def := fake.Int()
+		f := func() int {
+			return def
+		}
+
+		assert.Equal(t, def, r.UnwrapOrElse(f))
+	})
+}
+
+func TestResult_UnwrapOrDefault(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Equal(t, val, r.UnwrapOrDefault())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+
+		ri := newErr[int](err)
+		assert.Equal(t, 0, ri.UnwrapOrDefault())
+
+		rs := newErr[string](err)
+		assert.Empty(t, rs.UnwrapOrDefault())
+	})
+}
+
+func TestResult_UnwrapErr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		expected := fmt.Errorf("called `Result.UnwrapErr()` on an `Ok` value: %v", val)
+		assert.PanicsWithError(t, expected.Error(), func() {
+			_ = r.UnwrapErr()
+		})
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[any](err)
+
+		assert.Equal(t, err, r.UnwrapErr())
+	})
+}
+
+func TestResult_Inspect(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		called := false
+		p := func(v *int) {
+			called = true
+
+			assert.Equal(t, val, *v)
+		}
+
+		res := r.Inspect(p)
+
+		assert.Same(t, res, r)
+		assert.True(t, called, "predicate should have been called")
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		p := func(*int) {
+			assert.Fail(t, "predicate should not have been called")
+		}
+
+		res := r.Inspect(p)
+
+		assert.Same(t, res, r)
+	})
+}
+
+func TestResult_InspectErr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		p := func(error) {
+			assert.Fail(t, "predicate should not have been called")
+		}
+
+		res := r.InspectErr(p)
+
+		assert.Same(t, res, r)
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		called := false
+		p := func(e error) {
+			called = true
+
+			assert.Equal(t, err, e)
+		}
+
+		res := r.InspectErr(p)
+
+		assert.Same(t, res, r)
+		assert.True(t, called, "predicate should have been called")
+	})
+}
+
+func TestResult_AsOptionValue(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Equal(t, Some(val), r.AsOptionValue())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		assert.Equal(t, None[int](), r.AsOptionValue())
+	})
+}
+
+func TestResult_AsOptionErr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Equal(t, None[error](), r.AsOptionErr())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		assert.Equal(t, Some(err), r.AsOptionErr())
+	})
+}
+
+func TestResult_WrapErr(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		assert.Same(t, r, r.WrapErr(fake.RandomStringWithLength(8)))
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		msg := fake.RandomStringWithLength(8)
+		expected := newErr[int](fmt.Errorf("%s: %w", msg, err))
+		assert.Equal(t, expected, r.WrapErr(msg))
+	})
+}
+
+func TestResult_Expand(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		val := fake.Int()
+		r := newOk[int](val)
+
+		v, err := r.Expand()
+		require.NoError(t, err)
+		assert.Equal(t, val, v)
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		sourceErr := fmt.Errorf("some error: %s", fake.RandomStringWithLength(8))
+		r := newErr[int](sourceErr)
+
+		v, err := r.Expand()
+		require.ErrorContains(t, err, sourceErr.Error())
+		assert.Zero(t, v)
+	})
+}
+
+func TestResult_String(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		value := fake.Int()
+		r := newOk[int](value)
+
+		expected := fmt.Sprintf("Ok(%v)", value)
+
+		assert.Equal(t, expected, r.String())
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		err := errors.New(fake.RandomStringWithLength(8))
+		r := newErr[int](err)
+
+		expected := fmt.Sprintf("Err(%v)", err)
+		assert.Equal(t, expected, r.String())
+	})
+}
+
+func newOk[T any](val T) *result[T] {
+	r, ok := Ok(val).(*result[T])
+	if !ok {
+		panic("expected *result[T]")
+	}
+
+	return r
+}
+
+func newErr[T any](err error) *result[T] {
+	r, ok := Err[T](err).(*result[T])
+	if !ok {
+		panic("expected *result[T]")
+	}
+
+	return r
 }
