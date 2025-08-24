124ce806b6a1a5cda79f803881a88f6b042c0806
Mathieu Lemay <acidrain1@gmail.com>
2025-08-24 10:34:07 -0400

    Add missing doc for Option

--- safetypes/option.go
+++ safetypes/option.go
@@ -7,19 +7,36 @@ import (
 
 // Option is a type that represents either a value (Some) or not (None).
 type Option[T any] interface {
+	// IsNone returns true if the option is a None value.
 	IsNone() bool
+	// IsNoneOr returns true if the option is a None or the value inside of it matches a predicate.
 	IsNoneOr(f func(T) bool) bool
+	// IsSome returns true if the option is a Some value.
 	IsSome() bool
+	// IsSomeAnd returns true if the option is a Some and the value inside of it matches a predicate.
 	IsSomeAnd(f func(T) bool) bool
+	// Expect returns the contained Some value, consuming the self value. Panics if the value is a None with a custom
+	// panic message provided by msg.
 	Expect(msg string) T
+	// Unwrap returns the contained Some value, consuming the self value. Panics if the self value equals None.
 	Unwrap() T
+	// UnwrapOr returns the contained Some value or a provided default.
 	UnwrapOr(def T) T
+	// UnwrapOrElse returns the contained Some value or computes it from a closure.
 	UnwrapOrElse(f func() T) T
+	// UnwrapOrDefault returns the contained Some value or a default.
 	UnwrapOrDefault() T
+	// Inspect calls a function with a reference to the contained value if Some. Returns the original option.
 	Inspect(f func(T)) Option[T]
+	// Filter returns None if the option is None, otherwise calls predicate with the wrapped value and returns:
+	//  * Some(t) if predicate returns true (where t is the wrapped value), and
+	//  * None if predicate returns false.
 	Filter(f func(T) bool) Option[T]
+	// Or returns the option if it contains a value, otherwise returns optb.
 	Or(other Option[T]) Option[T]
+	// OrElse returns the option if it contains a value, otherwise calls f and returns the result.
 	OrElse(f func() Option[T]) Option[T]
+	// Xor returns Some if exactly one of self, optb is Some, otherwise returns None.
 	Xor(other Option[T]) Option[T]
 
 	fmt.Stringer
